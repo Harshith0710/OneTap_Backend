c1cfc777f732861cc9574cf9a35ab87f444b7c4c
Harshith0710 <challaharshith07@gmail.com>
2024-12-31 20:34:27 +0530

    added home screen ui part

--- app/src/main/java/com/example/geminiapikey/HomeActivity.kt
+++ app/src/main/java/com/example/geminiapikey/HomeActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.ui.platform.LocalContext
 
 class HomeActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
