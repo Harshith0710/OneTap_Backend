f694761e308527e75808a3504f6481c40870184a
Harshith0710 <challaharshith07@gmail.com>
2025-01-12 12:40:39 +0530

    added captions for ai speaking and updated speaking ui

--- app/src/main/java/com/example/geminiapikey/SpeakScreen.kt
+++ app/src/main/java/com/example/geminiapikey/SpeakScreen.kt
@@ -8,7 +8,6 @@ import android.speech.RecognizerIntent
 import android.speech.SpeechRecognizer
 import android.speech.tts.TextToSpeech
 import android.speech.tts.UtteranceProgressListener
-import android.util.Log
 import android.widget.Toast
 import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
@@ -19,12 +18,17 @@ import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.material3.Button
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.DisposableEffect
@@ -40,7 +44,10 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.drawscope.Stroke
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -79,12 +86,13 @@ fun RequestPermission(permission: String, onGranted: @Composable () -> Unit) {
 @Composable
 fun SpeakScreen(viewModel: BakingViewModel = viewModel()) {
     val context = LocalContext.current
-    var spokenText by remember { mutableStateOf("") } // This is where the recognized text will be stored
-    var partialText by remember { mutableStateOf("") } // For real-time partial results
+    var spokenText by remember { mutableStateOf("") }
+    var displayedText by remember { mutableStateOf("") }
+    var partialText by remember { mutableStateOf("") }
     var isListening by remember { mutableStateOf(false) }
     var amplitude by remember { mutableFloatStateOf(0.1f) }
     var isSpeaking by remember { mutableStateOf(false) }
-    var currentTTSChunk by remember { mutableStateOf("") } // For displaying TTS progress
+    var currentTTSChunk by remember { mutableStateOf("") }
 
     val tts = remember {
         TextToSpeech(context) { status ->
@@ -94,25 +102,22 @@ fun SpeakScreen(viewModel: BakingViewModel = viewModel()) {
         }.apply {
             setOnUtteranceProgressListener(object : UtteranceProgressListener() {
                 override fun onStart(utteranceId: String?) {
-                    Log.d("SpeakScreen", "TTS onStart")
                     isSpeaking = true
                 }
 
                 override fun onDone(utteranceId: String?) {
-                    Log.d("SpeakScreen", "TTS onDone")
                     isSpeaking = false
-                    currentTTSChunk = "" // Clear the current chunk when done
+                    currentTTSChunk = ""
+                    displayedText = "" // Clear displayed text when TTS is done
                 }
 
-                @Deprecated("Deprecated in Java")
                 override fun onError(utteranceId: String?) {
-                    Log.e("SpeakScreen", "TTS onError")
                     isSpeaking = false
-                    currentTTSChunk = "" // Clear the current chunk on error
+                    currentTTSChunk = ""
+                    displayedText = "" // Clear displayed text on TTS error
                 }
 
                 override fun onRangeStart(utteranceId: String?, start: Int, end: Int, frame: Int) {
-                    // Update the current TTS chunk in real-time
                     currentTTSChunk = viewModel.uiState.value.let {
                         when (it) {
                             is UiState.Success -> it.outputText.substring(start, end)
@@ -130,7 +135,7 @@ fun SpeakScreen(viewModel: BakingViewModel = viewModel()) {
     val speechRecognizerIntent = Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH).apply {
         putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL, RecognizerIntent.LANGUAGE_MODEL_FREE_FORM)
         putExtra(RecognizerIntent.EXTRA_LANGUAGE, Locale.getDefault())
-        putExtra(RecognizerIntent.EXTRA_PARTIAL_RESULTS, true) // Enable partial results
+        putExtra(RecognizerIntent.EXTRA_PARTIAL_RESULTS, true)
     }
 
     DisposableEffect(Unit) {
@@ -149,9 +154,7 @@ fun SpeakScreen(viewModel: BakingViewModel = viewModel()) {
             isListening = true
         }
 
-        override fun onRmsChanged(rmsdB: Float) {
-            // You can use rmsdB to adjust the amplitude in real-time if needed
-        }
+        override fun onRmsChanged(rmsdB: Float) {}
 
         override fun onBufferReceived(buffer: ByteArray?) {}
 
@@ -167,14 +170,14 @@ fun SpeakScreen(viewModel: BakingViewModel = viewModel()) {
         override fun onResults(results: Bundle?) {
             isListening = false
             val result = results?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)?.get(0) ?: ""
-            spokenText = result // Store the final recognized text here
-            partialText = "" // Clear partial text when final results are received
-            viewModel.sendPrompt(null, result) // Send user input to ViewModel
+            spokenText = result
+            displayedText = result
+            partialText = ""
+            viewModel.sendPrompt(null, result)
         }
 
         override fun onPartialResults(partialResults: Bundle?) {
-            val partialResult = partialResults?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)?.get(0) ?: ""
-            partialText = partialResult
+            partialText = partialResults?.getStringArrayList(SpeechRecognizer.RESULTS_RECOGNITION)?.get(0) ?: ""
         }
 
         override fun onEvent(eventType: Int, params: Bundle?) {}
@@ -183,46 +186,78 @@ fun SpeakScreen(viewModel: BakingViewModel = viewModel()) {
     Column(
         modifier = Modifier
             .fillMaxSize()
+            .background(Color(ContextCompat.getColor(context, R.color.splash_background)))
             .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        // Display TTS progress or recognition results
         Text(
-            text = when {
-                isSpeaking -> currentTTSChunk.ifEmpty { "Speaking..." }
-                isListening -> partialText.ifEmpty { "Listening..." }
-                else -> "Speak something!"
-            },
+            text = partialText,
+            color = Color.White,
             modifier = Modifier.padding(16.dp)
         )
 
-        AnimatedLines(
-            amplitude = if (isSpeaking) amplitude else 1f,
-            isSpeaking = isSpeaking
+        Text(
+            text = displayedText,
+            color = Color.White,
+            modifier = Modifier.padding(16.dp)
         )
 
-        Button(onClick = {
-            if (isListening) {
-                speechRecognizer.stopListening()
-            } else {
-                if (isSpeaking) {
-                    tts.stop() // Stop TTS if it is currently speaking
-                    isSpeaking = false
-                }
-                val hasPermission = ContextCompat.checkSelfPermission(
-                    context,
-                    android.Manifest.permission.RECORD_AUDIO
-                ) == android.content.pm.PackageManager.PERMISSION_GRANTED
+        AnimatedLines(amplitude = if (isSpeaking) amplitude else 1f, isSpeaking = isSpeaking)
 
-                if (hasPermission) {
-                    speechRecognizer.startListening(speechRecognizerIntent)
-                } else {
+        Text(
+            text = if (isSpeaking) currentTTSChunk else "",
+            color = Color.White,
+            modifier = Modifier.padding(16.dp)
+        )
+
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            Box(
+                modifier = Modifier.size(50.dp), // Set the size of the outer Box
+                contentAlignment = Alignment.Center // Center-align the content inside the Box
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.ellipse_84),
+                    contentDescription = "",
+                    modifier = Modifier.fillMaxSize() // Fill the outer Box
+                )
+                Image(
+                    painter = painterResource(R.drawable.keyboard_10_1),
+                    contentDescription = "",
+                    modifier = Modifier.size(25.dp) // Size for the inner image
+                )
+            }
+            CircularPulseAnimation(
+                isListening = isListening,
+                isSpeaking = isSpeaking,
+                speechRecognizer = speechRecognizer,
+                speechRecognizerIntent = speechRecognizerIntent,
+                tts = tts,
+                onPermissionDenied = {
                     Toast.makeText(context, "Permission denied. Please enable microphone access.", Toast.LENGTH_SHORT).show()
                 }
+            )
+            Box(
+                modifier = Modifier.size(50.dp), // Set the size of the outer Box
+                contentAlignment = Alignment.Center // Center-align the content inside the Box
+            ) {
+                Image(
+                    painter = painterResource(R.drawable.ellipse_85),
+                    contentDescription = "",
+                    modifier = Modifier.fillMaxSize() // Fill the outer Box
+                )
+                Image(
+                    painter = painterResource(R.drawable.fi_rr_cross_small_1_1),
+                    contentDescription = "",
+                    modifier = Modifier.size(25.dp) // Size for the inner image
+                )
             }
-        }) {
-            Text(if (isListening) "Stop Listening" else "Start Listening")
         }
     }
 
@@ -230,7 +265,6 @@ fun SpeakScreen(viewModel: BakingViewModel = viewModel()) {
         is UiState.Success -> {
             LaunchedEffect(state) {
                 amplitude = 0.6f
-                // Let TTS handle chunk-based updates
                 tts.speak(state.outputText, TextToSpeech.QUEUE_FLUSH, null, "utteranceId")
             }
         }
@@ -238,7 +272,6 @@ fun SpeakScreen(viewModel: BakingViewModel = viewModel()) {
         is UiState.Streaming -> {
             LaunchedEffect(state) {
                 amplitude = 0.6f
-                // Handle partial streaming dynamically
                 tts.speak(state.partialText, TextToSpeech.QUEUE_ADD, null, "utteranceId")
             }
         }
@@ -258,7 +291,7 @@ fun AnimatedLines(amplitude: Float, isSpeaking: Boolean) {
     val infiniteTransition = rememberInfiniteTransition(label = "Irregular Voice Lines")
     val lineCount = 10
     val staticAmplitudes = listOf(0.3f, 0.5f, 0.7f, 0.4f, 0.6f, 0.8f, 0.5f, 0.7f, 0.6f, 0.4f)
-    val lineHeights = if (isSpeaking){
+    val lineHeights = if (isSpeaking) {
         List(lineCount) {
             infiniteTransition.animateFloat(
                 initialValue = 0f,
@@ -299,6 +332,108 @@ fun AnimatedLines(amplitude: Float, isSpeaking: Boolean) {
                 cap = StrokeCap.Round
             )
         }
+    }
+}
+
+@Composable
+fun CircularPulseAnimation(
+    isListening: Boolean,
+    isSpeaking: Boolean,
+    speechRecognizer: SpeechRecognizer,
+    speechRecognizerIntent: Intent,
+    tts: TextToSpeech,
+    onPermissionDenied: () -> Unit
+) {
+    val context = LocalContext.current
+    val infiniteTransition = rememberInfiniteTransition(label = "")
+    val outerCircleAlpha by infiniteTransition.animateFloat(
+        initialValue = 0.3f,
+        targetValue = 0f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(1500, easing = LinearEasing),
+            repeatMode = RepeatMode.Restart
+        ), label = ""
+    )
+
+    val outerCircleSize by infiniteTransition.animateFloat(
+        initialValue = 0.8f,
+        targetValue = 1.2f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(1500, easing = LinearEasing),
+            repeatMode = RepeatMode.Restart
+        ), label = ""
+    )
+
+    val staticOuterCircleSize = 1f
+    val dotColor = Color.Magenta
+
+    Box(
+        modifier = Modifier
+            .size(200.dp),
+        contentAlignment = Alignment.Center
+    ) {
+        Canvas(modifier = Modifier.matchParentSize()) {
+            val maxRadius = size.minDimension / 2
+            val circlePadding = 5f
+            val circleRadii = listOf(1.0f, 0.8f, 0.6f)
+
+            circleRadii.forEach { scale ->
+                val alpha = if (isListening) {
+                    outerCircleAlpha
+                } else {
+                    when (scale) {
+                        1.0f -> 0.3f
+                        0.8f -> 0.5f
+                        0.6f -> 0.7f
+                        else -> 1f
+                    }
+                }
+
+                drawCircle(
+                    color = dotColor.copy(alpha = alpha),
+                    radius = maxRadius * scale * (if (isListening) outerCircleSize else staticOuterCircleSize) - circlePadding,
+                    style = Stroke(width = 5f)
+                )
+            }
+        }
+
+        Canvas(
+            modifier = Modifier
+                .size(80.dp)
+                .clickable {
+                    if (isListening) {
+                        speechRecognizer.stopListening()
+                    } else {
+                        if (isSpeaking) {
+                            tts.stop()
+                        }
+                        val hasPermission = ContextCompat.checkSelfPermission(
+                            context,
+                            android.Manifest.permission.RECORD_AUDIO
+                        ) == android.content.pm.PackageManager.PERMISSION_GRANTED
+
+                        if (hasPermission) {
+                            speechRecognizer.startListening(speechRecognizerIntent)
+                        } else {
+                            onPermissionDenied()
+                        }
+                    }
+                },
+            onDraw = {
+                val maxRadius = size.minDimension / 2
+                drawCircle(
+                    color = Color.White,
+                    radius = maxRadius
+                )
+            }
+        )
+
+        Image(
+            painter = painterResource(id = R.drawable.microphone_1),
+            contentDescription = "Microphone Icon",
+            modifier = Modifier.size(40.dp),
+            contentScale = ContentScale.Fit
+        )
     }
 }
 
